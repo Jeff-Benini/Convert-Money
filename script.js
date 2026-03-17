d78d8fe785946d96d07d26b976a0f891272104d8
Jéferson Benini <jefersonbenini@gmail.com>
2026-03-17 00:08:24 -0300

    Refactor currency conversion logic and functions

--- script.js
+++ script.js
@@ -1,175 +1,73 @@
 const convertButton = document.querySelector(".convert-button")
 const currencyConvertTo = document.querySelector(".select-currency-convert-to")
 const currencyConvertFrom = document.querySelector(".select-currency-convert-from")
+const inputValue = document.querySelector(".input-value")
+const valueToConvert = document.querySelector(".value-to-convert")
+const valueConverted = document.querySelector(".value-converted")
+
+const rates = {
+    real: 1,
+    dolar: 5.23,
+    euro: 6.02,
+    libra: 6.97
+}
+
+const formats = {
+    real: "pt-BR",
+    dolar: "en-US",
+    euro: "de-DE",
+    libra: "en-GB"
+}
+
+const currencyNames = {
+    real: "Real",
+    dolar: "Dólar",
+    euro: "Euro",
+    libra: "Libra"
+}
+
+function formatCurrency(value, currency) {
+    return new Intl.NumberFormat(formats[currency], {
+        style: "currency",
+        currency: currency === "real" ? "BRL" :
+                  currency === "dolar" ? "USD" :
+                  currency === "euro" ? "EUR" : "GBP"
+    }).format(value)
+}
 
 function convertValues() {
-    const inputValue = document.querySelector(".input-value").value
-    const valueToConvert = document.querySelector(".value-to-convert")
-    const valueConverted = document.querySelector(".value-converted")
+    const value = Number(inputValue.value)
 
-    const dolarToday = 5.23
-    const euroToday = 6.02
-    const libraToday = 6.97
+    const fromRate = rates[currencyConvertFrom.value]
+    const toRate = rates[currencyConvertTo.value]
 
-        let rateFrom = 1
+    const valueInReal = value * fromRate
+    const convertedValue = valueInReal / toRate
 
-        if (currencyConvertFrom.value === "dolar") {
-            rateFrom = dolarToday
-        }
-
-        if (currencyConvertFrom.value === "euro") {
-            rateFrom = euroToday
-        }
-
-        if (currencyConvertFrom.value === "libra") {
-            rateFrom = libraToday
-        }
-
-
-        const valueInReal = inputValue * rateFrom
-
-        if (currencyConvertTo.value == "dolar" ) {
-            valueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD"
-            }).format(valueInReal / dolarToday)
-        }
-
-        if (currencyConvertTo.value == "euro") {
-            valueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
-                style: "currency",
-                currency: "EUR"
-            }).format(valueInReal / euroToday)
-        }
-
-        if (currencyConvertTo.value == "libra") {
-            valueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
-                style: "currency",
-                currency: "GBP"
-            }).format(valueInReal / libraToday)
-        }
-
-
-        if (currencyConvertTo.value == "real") {
-        valueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL"
-        }).format(inputValue)
-        }
-
-    if (currencyConvertFrom.value == "dolar" ) {
-            valueToConvert.innerHTML = new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD"
-            }).format(inputValue)
-        }
-
-        if (currencyConvertFrom.value == "euro") {
-            valueToConvert.innerHTML = new Intl.NumberFormat("de-DE", {
-                style: "currency",
-                currency: "EUR"
-            }).format(inputValue)
-        }
-
-        if (currencyConvertFrom.value == "libra") {
-            valueToConvert.innerHTML = new Intl.NumberFormat("en-GB", {
-                style: "currency",
-                currency: "GBP"
-            }).format(inputValue)
-        }
-
-
-        if (currencyConvertFrom.value == "real") {
-        valueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL"
-        }).format(inputValue)
-    }
+    valueToConvert.innerHTML = formatCurrency(value, currencyConvertFrom.value)
+    valueConverted.innerHTML = formatCurrency(convertedValue, currencyConvertTo.value)
 }
 
 function changeCurrencyFrom() {
-            if (currencyConvertFrom.value == "real") {
-                const logoCurrencyTo = document.querySelector(".logo-currency-from")
-                const currencyToConvert = document.querySelector(".currency-to-convert")
+    const logo = document.querySelector(".logo-currency-from")
+    const currencyText = document.querySelector(".currency-to-convert")
 
-                currencyToConvert.innerHTML = "Real"
-                logoCurrencyTo.src = "assets/real.png"
-            }
+    currencyText.innerHTML = currencyNames[currencyConvertFrom.value]
+    logo.src = `assets/${currencyConvertFrom.value}.png`
 
-            if (currencyConvertFrom.value == "dolar") {
-                const logoCurrencyTo = document.querySelector(".logo-currency-from")
-                const currencyToConvert = document.querySelector(".currency-to-convert")
+    convertValues()
+}
 
-                currencyToConvert.innerHTML = "Dólar"
-                logoCurrencyTo.src = "assets/dolar.png"
-            }
+function changeCurrencyTo() {
+    const logo = document.querySelector(".logo-currency-to")
+    const currencyText = document.querySelector(".currency-converted")
 
-            if (currencyConvertFrom.value == "euro") {
-                const logoCurrencyTo = document.querySelector(".logo-currency-from")
-                const currencyToConvert = document.querySelector(".currency-to-convert")
+    currencyText.innerHTML = currencyNames[currencyConvertTo.value]
+    logo.src = `assets/${currencyConvertTo.value}.png`
 
-                currencyToConvert.innerHTML = "Euro"
-                logoCurrencyTo.src = "assets/euro.png"
-            }
-
-            if (currencyConvertFrom.value == "libra") {
-                const logoCurrencyTo = document.querySelector(".logo-currency-from")
-                const currencyToConvert = document.querySelector(".currency-to-convert")
-
-                currencyToConvert.innerHTML = "Libra"
-                logoCurrencyTo.src = "assets/libra.png"
-            }
-
-
-            convertValues()
-        }
-
-        function changeCurrencyTo() {
-            if (currencyConvertTo.value == "real") {
-                const logoCurrencyTo = document.querySelector(".logo-currency-to")
-                const currencyConverted = document.querySelector(".currency-converted")
-
-                currencyConverted.innerHTML = "Real"
-                logoCurrencyTo.src = "assets/real.png"
-            }
-
-            if (currencyConvertTo.value == "dolar") {
-                const logoCurrencyTo = document.querySelector(".logo-currency-to")
-                const currencyConverted = document.querySelector(".currency-converted")
-
-                currencyConverted.innerHTML = "Dólar"
-                logoCurrencyTo.src = "assets/dolar.png"
-            }
-
-            if (currencyConvertTo.value == "euro") {
-                const logoCurrencyTo = document.querySelector(".logo-currency-to")
-                const currencyConverted = document.querySelector(".currency-converted")
-
-                currencyConverted.innerHTML = "Euro"
-                logoCurrencyTo.src = "assets/euro.png"
-            }
-
-            if (currencyConvertTo.value == "libra") {
-                const logoCurrencyTo = document.querySelector(".logo-currency-to")
-                const currencyConverted = document.querySelector(".currency-converted")
-
-                currencyConverted.innerHTML = "Libra"
-                logoCurrencyTo.src = "assets/libra.png"
-            }
-
-
-            convertValues()
-        }
+    convertValues()
+}
 
 currencyConvertTo.addEventListener("change", changeCurrencyTo)
 currencyConvertFrom.addEventListener("change", changeCurrencyFrom)
 convertButton.addEventListener("click", convertValues)
-
-
-
-
-
-    
-
-
-    
